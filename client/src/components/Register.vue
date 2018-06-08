bcded188ddf53818fac984dedd1bc954ed8977d0
subba Rao <login4subba@gmail.com>
2018-06-08 22:58:15 +0530

    this is first

---
+++ client/src/components/Register.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+<label for="email">Email:</label>
+<input v-model="email" id="email" type="email">
+<label for="password">Password:</label>
+<input v-model="password" id="password" type="password">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
